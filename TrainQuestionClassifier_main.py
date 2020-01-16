4b531fe48d4de3a354cf2fd071edd66a39f4fbf3
Andy Lui <52951273+luikat22@users.noreply.github.com>
2020-01-16 10:51:38 +0800

    Create TrainQuestionClassifier_main.py

---
+++ TrainQuestionClassifier_main.py
@@ -0,0 +1,13 @@
+import training_files.TrainQuestionClassifier_model1 as train_question_classifier_in_one_word
+import training_files.TrainQuestionClassifier_model2 as train_question_classifier_in_two_word
+import training_files.TrainQuestionClassifier_model3 as train_question_classifier_in_three_word
+
+
+print('----- !!!!! Running the train_question_classifier_in_one_word !!!!! -----')
+exec(train_question_classifier_in_one_word)
+
+print('----- !!!!! Running the train_question_classifier_in_two_word !!!!! -----')
+exec(train_question_classifier_in_two_word)
+
+print('----- !!!!! Running the train_question_classifier_in_three_word !!!!! -----')
+exec(train_question_classifier_in_three_word)
